c9365225e78678f026ebf411dfa576cdebccc920
vladosby <vladislavn.kievski@gmail.com>
2017-01-22 02:24:29 +0300

    Adding new component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Hero } from 'Hero';
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -20,21 +20,15 @@ const HEROES: Hero[] = [
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-  <li *ngFor="let hero of heroes"
-    [class.selected]="hero === selectedHero"
-    (click)="onSelect(hero)">
-    <span class="badge">{{hero.id}}</span> {{hero.name}}
-  </li>
+    <li *ngFor="let hero of heroes"
+      [class.selected]="hero === selectedHero"
+      (click)="onSelect(hero)">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
   </ul>
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
-  `, styles: [`
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+`,
+ styles: [`
   .selected {
     background-color: #CFD8DC !important;
     color: white;
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @NgModule({
   imports: [
@@ -9,8 +11,10 @@ import { AppComponent }  from './app.component';
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
