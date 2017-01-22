69da09b277aad0a95884bf489916fc7c119e13cd
vladosby <vladislavn.kievski@gmail.com>
2017-01-22 15:03:45 +0300

    Add styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+
+//without moduleId: module.id won't find css and html files
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
   template: `
-   <h1>{{title}}</h1>
-   <nav>
-     <a routerLink="/dashboard">Dashboard</a>
-     <a routerLink="/heroes">Heroes</a>
-   </nav>
-   <router-outlet></router-outlet>
- `
+  <h1>{{title}}</h1>
+  <nav>
+    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
+  styleUrls: [ 'app.component.css' ]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
